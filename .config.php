8e87217551d3a061608965737209e66680e43d00
Alexey Plodenko <alexey@plodenko.com>
2022-11-06 11:33:56 +0200

    Simplified the configuration and moved the base config. file to the top level.

---
+++ .config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'servers' => [
+        'Default' => [
+            'abbywinters-memcached:11200' => [
+                'hostname' => 'host.docker.internal',
+                'port' => '11200',
+            ],
+        ],
+    ],
+];
